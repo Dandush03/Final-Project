f79a1f82531358b88bd3992bc69feb03ae541d5b
Daniel Laloush <d.laloush@outlook.com>
2020-07-30 11:56:20 -0400

    Fix Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
     render file: 'public/index.html'
   end
 
-  def after_sign_out_path_for(resource)
+  def after_sign_out_path_for(_resource)
     root_path
   end
 
